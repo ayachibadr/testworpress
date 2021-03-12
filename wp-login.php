b9e4e6cc7cae8321af0ee8dafc0b4c909c790845
ayachibadr <49597400+ayachibadr@users.noreply.github.com>
2021-03-12 23:00:54 +0100

    REFRESH

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,11 @@
 	<title>Se connecter &lsaquo; farouha MY LIFE &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="/https://ayachibadr.github.io/testworpress/wp-includes/css/dashicons.min.css?ver=5.7" media="all">
-<link rel="stylesheet" id="buttons-css" href="/https://ayachibadr.github.io/testworpress/wp-includes/css/buttons.min.css?ver=5.7" media="all">
-<link rel="stylesheet" id="forms-css" href="/https://ayachibadr.github.io/testworpress/wp-admin/css/forms.min.css?ver=5.7" media="all">
-<link rel="stylesheet" id="l10n-css" href="/https://ayachibadr.github.io/testworpress/wp-admin/css/l10n.min.css?ver=5.7" media="all">
-<link rel="stylesheet" id="login-css" href="/https://ayachibadr.github.io/testworpress/wp-admin/css/login.min.css?ver=5.7" media="all">
+<link rel="stylesheet" id="dashicons-css" href="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/css/dashicons.min.css?ver=5.7" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/css/buttons.min.css?ver=5.7" media="all">
+<link rel="stylesheet" id="forms-css" href="http://localhost/https:/ayachibadr.github.io/testworpress/wp-admin/css/forms.min.css?ver=5.7" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://localhost/https:/ayachibadr.github.io/testworpress/wp-admin/css/l10n.min.css?ver=5.7" media="all">
+<link rel="stylesheet" id="login-css" href="http://localhost/https:/ayachibadr.github.io/testworpress/wp-admin/css/login.min.css?ver=5.7" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://fr.wordpress.org/">Propulsé par WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/https://ayachibadr.github.io/testworpress/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="http://localhost/https:/ayachibadr.github.io/testworpress/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Identifiant ou adresse e-mail</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -44,29 +44,29 @@
 		</form>
 
 					<p id="nav">
-								<a href="/https://ayachibadr.github.io/testworpress/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
+								<a href="http://localhost/https:/ayachibadr.github.io/testworpress/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="/https://ayachibadr.github.io/testworpress/">&larr; Aller sur farouha MY LIFE</a>		</p>
+			<a href="http://localhost/https:/ayachibadr.github.io/testworpress/">&larr; Aller sur farouha MY LIFE</a>		</p>
 			</div>
-	<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+	<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script id="zxcvbn-async-js-extra">
 var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
 <script id="wp-polyfill-js-after">
 ( 'fetch' in window ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
 </script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
-wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
 <script id="password-strength-meter-js-extra">
 var pwsL10n = {"unknown":"Force du mot de passe inconnue.","short":"Très faible","bad":"Faible","good":"Moyenne","strong":"Forte","mismatch":"Non concordance"};
@@ -78,12 +78,12 @@ var pwsL10n = {"unknown":"Force du mot de passe inconnue.","short":"Très faible
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-03-10 12:30:07+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"fr"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s est obsolète depuis la version %2$s ! Utilisez %3$s à la place. Pensons à écrire du code plus inclusif."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-admin/js/password-strength-meter.min.js?ver=5.7" id="password-strength-meter-js"></script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-admin/js/password-strength-meter.min.js?ver=5.7" id="password-strength-meter-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
 <script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/testworpress\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-includes/js/wp-util.min.js?ver=5.7" id="wp-util-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-includes/js/wp-util.min.js?ver=5.7" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
 var userProfileL10n = {"user_id":"0","nonce":"8afe0da542"};
 </script>
@@ -94,7 +94,7 @@ var userProfileL10n = {"user_id":"0","nonce":"8afe0da542"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-03-10 12:30:07+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"fr"},"Your new password has not been saved.":["Votre nouveau mot de passe n’a pas été enregistré."],"Hide":["Masquer"],"Show":["Afficher"],"Confirm use of weak password":["Confirmer l’utilisation du mot de passe faible"],"Hide password":["Cacher le mot de passe"],"Show password":["Afficher le mot de passe"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script src="/https://ayachibadr.github.io/testworpress/wp-admin/js/user-profile.min.js?ver=5.7" id="user-profile-js"></script>
+<script src="http://localhost/https:/ayachibadr.github.io/testworpress/wp-admin/js/user-profile.min.js?ver=5.7" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
